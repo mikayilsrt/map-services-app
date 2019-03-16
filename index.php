e96471d56b3e81d8efb4ffb88972f6600066c6c1
Mikayil S <sert.mikayil@gmail.com>
2019-03-17 00:17:36 +0100

    style: update style.css and add roboto font

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $faker = Faker\Factory::create();
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="./assets/css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
         <title>Map services app</title>
     </head>
     <body>
