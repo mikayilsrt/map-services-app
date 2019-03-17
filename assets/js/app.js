3aff7ed763e60ba5fc4096455bb14baaedc18e6b
Mikayil S <sert.mikayil@gmail.com>
2019-03-17 19:31:25 +0100

    feat: add google map and script.min.js library.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
-console.log('Hello world')
+const $map = document.getElementById('map')
+
+class GoogleMap {
+
+    constructor () {
+        this.map = null
+    }
+
+    loadMap (element) {
+        $script('https://maps.googleapis.com/maps/api/js', () => {
+            this.map = new google.maps.Map(element, {
+                center: {lat: -34.397, lng: 150.644},
+                zoom: 8
+            });
+        })
+    }
+
+}
+
+if ($map !== null) {
+    let map = new GoogleMap()
+    map.loadMap($map)
+}
