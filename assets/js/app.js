d2e5f958c8c34d4519c33bbf7b66a77792b5ff61
Mikayil S <sert.mikayil@gmail.com>
2019-03-19 21:48:25 +0100

    fix: fix a error

--- assets/js/app.js
+++ assets/js/app.js
@@ -49,18 +49,43 @@ class GoogleMap {
         this.map.fitBounds(this.bounds)
     }
 
+    /**
+     * Center the map on marker after click.
+     * @param { JSON } latLng 
+     */
+    centerOnMarker (latLng) {
+        new google.maps.Map($map, {
+            zoom: 4,
+            center: latLng
+        })
+    }
+
 }
 
 if ($map !== null) {
     let map = new GoogleMap()
     map.loadMap($map).then(() => {
-        Array.from(document.querySelectorAll('.js-item')).forEach((item) => {
+
+        let items = document.querySelectorAll('.js-item')
+
+        Array.from(items).forEach((item) => {
         
             let lat = item.dataset.lat
             let lng = item.dataset.lng
             let name = item.dataset.name
             
             map.addMarker(lat, lng, name)
+            
+            // zoom on marker after click a item.
+            item.addEventListener('click', (e) => {
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].classList[2] === 'active') {
+                        items[i].classList.remove('active')
+                    }
+                }
+                item.classList.add('active')
+                map.centerOnMarker({ lat: parseInt(item.dataset.lat), lng: parseInt(item.dataset.lng) })
+            })
         })
         map.centerMap()
     })
