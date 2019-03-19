bfa12d2ec62b98692df8f61372507dff6a0c31ba
Mikayil S <sert.mikayil@gmail.com>
2019-03-19 21:21:50 +0100

    feat: add zoom on marker event

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,8 @@ class GoogleMap {
      */
     async loadMap (element) {
         return new Promise((resolve, reject) => {
-            $script('https://maps.googleapis.com/maps/api/js', () => {
+            let key = "AIzaSyBGd_dSiaAEDVv3WoJLbM2clVgLmGLhRz8"
+            $script('https://maps.googleapis.com/maps/api/js?key=' + key, () => {
                 this.map = new google.maps.Map(element)
                 this.bounds = new google.maps.LatLngBounds()
                 resolve()
@@ -49,18 +50,43 @@ class GoogleMap {
         this.map.fitBounds(this.bounds)
     }
 
+    /**
+     * Center the map on marker after click.
+     * @param { JSON } latLng 
+     */
+    centerOnMarker (latLng) {
+        new google.maps.Map($map, {
+            zoom: 4,
+            center: latLng
+        })
+    }
+
 }
 
 if ($map !== null) {
     let map = new GoogleMap()
     map.loadMap($map).then(() => {
-        Array.from(document.querySelectorAll('.js-item')).forEach((item) => {
+
+        let items = document.querySelectorAll('.js-item')
+
+        Array.from(items).forEach((item) => {
         
             let lat = item.dataset.lat
             let lng = item.dataset.lng
             let name = item.dataset.name
             
             map.addMarker(lat, lng, name)
+            
+            // zoom on marker after click a item.
+            item.addEventListener('click', (e) => {
+                for (var i = 0; i < items.length; i++) {
+                    if (items[i].classList[2] === 'active') {
+                        items[i].classList.remove('active')
+                    }
+                }
+                item.classList.add('active')
+                map.centerOnMarker({ lat: parseInt(item.dataset.lat), lng: parseInt(item.dataset.lng) })
+            })
         })
         map.centerMap()
     })
